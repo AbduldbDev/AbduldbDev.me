f3ad3ff7bfa1f1a4c71900338e2180459344d294
Abdul De Borja <abduldbdev@gmail.com>
2025-10-31 20:35:16 +0800

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const LandingPage = ({ showWelcome, setShowWelcome }) => (
 
     {!showWelcome && (
       <>
-        {/* <VisitNotifier /> */}
+        <VisitNotifier />
         <Navbar />
         <AnimatedBackground />
         <Home />
