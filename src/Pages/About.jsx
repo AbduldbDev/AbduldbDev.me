3e304e3a5a738bcedff117e8d02999023a28b8ee
Abdul De Borja <abduldbdev@gmail.com>
2025-11-04 13:31:56 +0800

    Update CV

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -281,7 +281,7 @@ const AboutPage = () => {
             </div>
 
             <div className="flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-4 lg:px-0 w-full">
-              <a href="/CV_DeBorja.pdf" download className="w-full lg:w-auto">
+              <a href="/CV_DeBorjaaa.pdf" download className="w-full lg:w-auto">
                 <button
                   data-aos="fade-up"
                   data-aos-duration="800"
